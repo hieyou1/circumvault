643afe345d922fd43c0c84512f951f6c9886cf62
hieyou1 <31020711+hieyou1@users.noreply.github.com>
2018-06-09 16:29:54 -0500

    nodefiles

--- index.js
+++ index.js
@@ -4,10 +4,7 @@ var homedir = require('os').homedir();
 if (thing) {
   console.log("Everything is properly installed and in the right place. You should be good to go!");
 } else {
-  var thething = JSON.parse(fs.readFileSync(homedir + '/circumvaulttemp.json', 'utf8'))
-  .catch({
-    throw new Error("FATAL: Not a file path");
-  });
+  var thething = JSON.parse(fs.readFileSync(homedir + '/circumvaulttemp.json', 'utf8'));
   var exec = thething.CFBundleExecutable;
   console.log(exec);
 }
