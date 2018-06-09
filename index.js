a15c3a4f5098517dd497a75c471849ea5b3a85de
hieyou1 <31020711+hieyou1@users.noreply.github.com>
2018-06-09 16:26:22 -0500

    nodefiles

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const fs = require('fs');
+const thing = process.argv[2];
 var homedir = require('os').homedir();
-var thething = JSON.parse(fs.readFileSync(homedir + '/circumvaulttemp.json', 'utf8'));
-var exec = thething.CFBundleExecutable;
-console.log(exec);
+if (thing) {
+  console.log("Everything is properly installed and in the right place. You should be good to go!");
+} else {
+  var thething = JSON.parse(fs.readFileSync(homedir + '/circumvaulttemp.json', 'utf8'));
+  var exec = thething.CFBundleExecutable;
+  console.log(exec);
+}
